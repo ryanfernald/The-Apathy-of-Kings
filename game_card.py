a09f03116fc65ce82a587099886bcafdfd42804c
Eric <158229984+EricSJSU-DataScience@users.noreply.github.com>
2024-11-25 22:57:24 -0800

    update: fix some small bug
    to-do: fix gamestate_update_move()

---
+++ game_card.py
@@ -0,0 +1,196 @@
+from enum import Enum
+
+
+
+class GameCard:
+    def __init__(self, argName: str, argType: int, argPath):
+        self.__name = argName   # card name
+        self.__type = CardType(argType)   # card type: attack card/ defense card/ support card
+        self.__imgPath = argPath
+        self.__view = CardView.BACK
+    
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def type(self):
+        return self.__type
+    @property
+    def imgPath(self):
+        return self.__imgPath
+    @property
+    def view(self):
+        return self.__view
+    @name.setter
+    def name(self, argName: str):
+        self.__name = argName
+    @type.setter    
+    def type(self, argType: int):
+        self.__type = argType
+    @imgPath.setter
+    def imgPath(self, argPath):
+        self.__imgPath = argPath
+    @view.setter
+    def view(self, argView):
+        self.__view = argView
+
+    def flip(self):
+        if self.__view == CardView.FRONT:
+            self.__view = CardView.BACK
+        else: 
+            self.__view = CardView.FRONT
+
+    def stateParsing(self, argString: str):
+        res = argString.split('.')[0].split(',')
+        return res
+
+    def display(self):
+        print('-' * 8 + ' Card Status ' + '-' * 8)
+        print(f'Name: {self.__name}')
+        print(f'Type: {self.__type}')
+
+    def info(self):
+        res = []
+        res.append(f'Name: {self.__name}')
+        res.append(f'Type: {self.__type}')
+        return res
+
+class GameCardAtk(GameCard):
+    def __init__(self, argString: str, argPath):
+        temp = super().stateParsing(argString)
+        super().__init__(temp[0], int(temp[1]), argPath)
+        self.__star = int(temp[2])
+        self.__element = CardElement(int(temp[3]))
+        self.__atk = int(temp[4])
+        self.__def = int(temp[5])
+        self.__hp = self.__def
+    
+    @property
+    def star(self):
+        return self.__star
+    @property
+    def element(self):
+        return self.__element
+    @property
+    def attack(self):
+        return self.__atk
+    @property
+    def defense(self):
+        return self.__def
+    @property
+    def hp(self):
+        return self.__hp
+    @hp.setter
+    def hp(self, update: int):
+        self.__hp = update
+
+    def display(self):
+        super().display()
+        print(f'Star: {self.__star}')
+        print(f'Element: {self.__element}')
+        print(f'Attack: {self.__atk}')
+        print(f'Defense: {self.__def}')
+        print(f'HP: {self.__hp}')
+    
+    def info(self):
+        res = []
+        res.extend(super().info())
+        res.append(f'Star: {self.__star}')
+        res.append(f'Element: {self.__element}')
+        res.append(f'Attack: {self.__atk}')
+        res.append(f'Defense: {self.__def}')
+        res.append(f'HP: {self.__hp}')
+        return res
+    
+    def __repr__(self):
+        """
+        show a string when print an object # not useful currently, could be delete
+        """
+        return (
+            f"{self.__class__.__name__}("
+            f"name='{self.name}', "
+            f"card_type={self.type.value}, "
+            # f"img_path='{self.imgPath}', " # does not require verify at the moment
+            f"star={self.__star}, "
+            f"element=CardElement({self.__element.value}), "
+            f"attack={self.__atk}, "
+            f"defense={self.__def}, "
+            f"HP={self.__hp})"
+        )
+
+class GameCardSup(GameCard):
+    def __init__(self, argString: str, argPath):
+        temp = super().stateParsing(argString)
+        super().__init__(temp[0], int(temp[1]), argPath)
+    
+class GameCardDef(GameCard):
+    def __init__(self, argString: str, argPath):
+        temp = super().stateParsing(argString)
+        super().__init__(temp[0], int(temp[1]), argPath)
+        self.__def = int(temp[2])
+        self.__hp = self.__def
+    
+    @property
+    def defense(self):
+        return self.__def
+    @property
+    def hp(self):
+        return self.__hp
+    @hp.setter
+    def hp(self, update: int):
+        self.__hp = update
+
+    def display(self):
+        super().display()
+        print(f'Defense: {self.__def}')
+        print(f'HP: {self.__hp}')
+    
+    def info(self):
+        res = []
+        res.extend(super().info())
+        res.append(f'Defense: {self.__def}')
+        res.append(f'HP: {self.__hp}')
+        return res
+    
+    def __repr__(self):
+        """
+        show a string when print an object # not useful currently, could be delete
+        """
+        return (
+            f"{self.__class__.__name__}("
+            f"name='{self.name}', "
+            f"card_type={self.type.value}, "
+            # f"img_path='{self.imgPath}', " # does not require verify at the moment
+            f"defense={self.__def}, "
+            f"HP={self.__hp})"
+        )
+
+# not sure use CardType or not for now
+class CardType(Enum):
+    ATTACK =1
+    SUPPORT = 2
+    DEFENSE = 3
+
+# not sure use CardElement or not for now
+class CardElement(Enum):    
+    WIND = 1
+    FIRE = 2
+    EARTH = 3
+    WATER = 4
+    ELECTRO = 5
+
+# tracking Card front image or back image
+class CardView(Enum):
+    FRONT = 1
+    BACK = 0
+
+# test
+if __name__ == "__main__":
+    print('-' * 10 + ' Test ' + '-' * 10)
+    path1 = 'd:\\Documents\\Code\\2024Fall\\CS122_Project\\The-Apathy-of-Kings-main\\modules\\assets\\cards\\Fire Attack Cards\\Emberbloom,1,4,2,900,700.png'
+    game_str = path1.split('\\')[-1]
+    card1 = GameCardAtk(game_str, path1)
+    print(card1.type)
+    print(card1.view)
+
+    print('-' * 10 + ' End ' + '-' * 10)
---
+++ game_play.py
@@ -0,0 +1,68 @@
+import random
+from . import utility as util
+
+
+class GamePlay:
+
+    def __init__(self):
+        self.__info = {
+            'player1': {'hand': [], 'deck': [], 'dragon': None},
+            'player2': {'hand': [], 'deck': [], 'dragon': None},
+        }
+        
+        # load card from \asset\card
+        card_atk_list, card_def_list, card_sup_list = util.load_cards_name_from_assets()
+        game_card_atk = util.convert_to_atk_card(card_atk_list)
+        game_card_def = util.convert_to_def_card(card_def_list)
+        game_card_sup = util.convert_to_sup_card(card_sup_list)
+
+        # Load dragon data
+        dragons = util.load_dragons_from_assets()
+        
+        # Assign dragons randomly
+        self.assign_dragons(dragons)
+
+        # set up card for player1
+        self.add_card(4, game_card_atk.copy(), self.__info['player1'])
+        self.add_card(2, game_card_def.copy(), self.__info['player1'])
+        self.add_card(1, game_card_sup.copy(), self.__info['player1'])
+        # set up card for player2
+        self.add_card(4, game_card_atk.copy(), self.__info['player2'])
+        self.add_card(2, game_card_def.copy(), self.__info['player2'])
+        self.add_card(1, game_card_sup.copy(), self.__info['player2'])
+
+
+    def assign_dragons(self, dragons):
+        if len(dragons) < 2:
+            raise ValueError("Not enough dragons available to assign to players!")
+        random.shuffle(dragons)
+        self.__info['player1']['dragon'] = dragons.pop()
+        self.__info['player2']['dragon'] = dragons.pop()
+
+        
+    @property
+    def info(self):
+        return self.__info
+
+    def add_card(self, num: int, cards: list, player: dict):
+        for itr in range(num):
+            drawn_card = random.choice(cards)
+            player['hand'].append(drawn_card)
+            cards.remove(drawn_card)
+        
+        player['deck'].extend(cards)
+        random.shuffle(player['deck'])
+    
+    # testing purpose
+    def displayGameInfo(self):
+        print(f'Player 1 hand: {len(self.__info["player1"]["hand"])}')
+        print(f'Player 1 deck: {len(self.__info["player1"]["deck"])}')
+        print(f'Player 2 hand: {len(self.__info["player2"]["hand"])}')
+        print(f'Player 2 deck: {len(self.__info["player2"]["deck"])}')
+
+# 
+if __name__ == "__main__":
+    game1 = GamePlay()
+    game1.displayGameInfo()
+
+
