71535b602ab7cfbc06bb86762fa223dc609457e4
Ryan <60462881+ryanfernald@users.noreply.github.com>
2024-12-02 23:28:02 -0800

    Clean Up
    Remove Old Beta Files

--- modules/resize_img.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image, ImageTk
-
-def resize_image(img_path, target_width, target_height):
-    img = Image.open(img_path)
-    resized_img = img.resize((target_width, target_height), Image.Resampling.LANCZOS)
-    return ImageTk.PhotoImage(resized_img)
